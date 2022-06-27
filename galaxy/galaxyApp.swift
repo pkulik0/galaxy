9da2a6acc51fc47447794b15198c2783fa68a344
pkulik0 <pkulik0@icloud.com>
2022-06-27 22:15:59 +0200

    Initial Commit

---
+++ galaxy/galaxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  galaxyApp.swift
+//  galaxy
+//
+//  Created by pkulik0 on 27/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct galaxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
