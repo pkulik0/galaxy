0fd3932880428f3debbed1c31f11dc1164e6bdd5
pkulik0 <pkulik0@icloud.com>
2022-07-19 00:22:50 +0200

    server: Add streamlink args

--- server/main.py
+++ server/main.py
@@ -1,8 +1,12 @@
-from flask import Flask
-import streamlink
 import json
+import streamlink
+from flask import Flask
+
 
 app = Flask(__name__)
+session = streamlink.Streamlink()
+session.set_plugin_option("twitch", "low-latency", True)
+
 
 @app.route("/")
 def index():
@@ -11,7 +15,7 @@ def index():
 
 @app.route("/<string:channel>")
 def get_channel(channel):
-    streams = streamlink.streams(f"https://twitch.tv/{channel}")
+    streams = session.streams(f"https://twitch.tv/{channel}")
 
     video_urls = {}
     for entry in streams:
