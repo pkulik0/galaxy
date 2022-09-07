ad439843313cecf2b587bff6e298c2a1b3af2c85
pkulik0 <pkulik0@icloud.com>
2022-09-07 19:09:39 +0200

    Remove server from repo

---
+++ galaxy/Views/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  galaxy
+//
+//  Created by pkulik0 on 25/08/2022.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
--- server/start.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-FLASK_APP=main python3 -m flask run --host 0.0.0.0
